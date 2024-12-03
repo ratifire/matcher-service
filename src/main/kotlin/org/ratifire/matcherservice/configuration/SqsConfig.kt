ecbff5e0f7c22c39d5d0f2127439cc5336c1f7ce
shaitan <shaitanaleksender@gmail.com>
2024-12-03 21:21:35 +0200

    added profiles

--- src/main/kotlin/org/ratifire/matcherservice/configuration/SqsConfig.kt
+++ src/main/kotlin/org/ratifire/matcherservice/configuration/SqsConfig.kt
@@ -3,6 +3,7 @@ package org.ratifire.matcherservice.configuration
 import io.awspring.cloud.sqs.operations.SqsTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
@@ -14,6 +15,7 @@ import java.net.URI
 class SqsConfig { // need to be reconfigured for prod env
 
     @Bean
+    @Profile("!prod")
     fun amazonSQSAsync(): SqsAsyncClient {
         return SqsAsyncClient.builder()
             .endpointOverride(URI.create("http://localhost:9324"))

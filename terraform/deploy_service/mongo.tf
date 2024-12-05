4615137be36c8f79e78b5d14b65cb9d142d3214b
NewEnergyCode <matveychuk.m@gmail.com>
2024-12-05 19:06:36 +0200

    Enable MongoDB Atlas provider and resources
    Uncommented and activated the MongoDB Atlas provider configuration and associated resources in the Terraform scripts. This change ensures that database users and IP access lists are properly managed through Terraform for better deployment consistency and automation.

--- terraform/deploy_service/mongo.tf
+++ terraform/deploy_service/mongo.tf
@@ -1,21 +1,21 @@
 
-# resource "mongodbatlas_database_user" "db_user" {
-#   project_id = var.project_id
-#   username   = "testuser"
-#   password   = var.db_user_password
-#   roles {
-#     role_name     = "testRole"
-#     database_name = "matcherMongoDB"
-#   }
-#   auth_database_name = "matcherMongoDB"
-# }
-#
-# resource "mongodbatlas_project_ip_access_list" "whitelist" {
-#   project_id = var.project_id
-#   cidr_block = "0.0.0.0/0"
-# }
-#
-# provider "mongodbatlas" {
-#   public_key  = var.mongodb_public_key
-#   private_key = var.mongodb_private_key
-# }
+resource "mongodbatlas_database_user" "db_user" {
+  project_id = var.project_id
+  username   = "testuser"
+  password   = var.db_user_password
+  roles {
+    role_name     = "testRole"
+    database_name = "matcherMongoDB"
+  }
+  auth_database_name = "matcherMongoDB"
+}
+
+resource "mongodbatlas_project_ip_access_list" "whitelist" {
+  project_id = var.project_id
+  cidr_block = "0.0.0.0/0"
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodb_public_key
+  private_key = var.mongodb_private_key
+}

d09cc3af201133e1babdbcd37495d91dec764f62
NewEnergyCode <matveychuk.m@gmail.com>
2024-12-05 18:38:50 +0200

    Normalize indentation in mongo.tf
    Adjusted the spacing of the Terraform configuration in mongo.tf to improve readability and maintain consistency. This change does not affect the functionality or configuration of the deployed resources.

--- terraform/deploy_service/mongo.tf
+++ terraform/deploy_service/mongo.tf
@@ -1,21 +1,21 @@
 resource "mongodbatlas_cluster" "mongo_cluster" {
   name                        = var.matcher_cluster_name
-  cluster_type                = "SHARED"
   provider_name               = "AWS"
   provider_region_name        = "EU_NORTH_1"
-  provider_instance_size_name = "M0"
   project_id                  = var.project_id
+  provider_instance_size_name = "M0"
 }
+
 
 resource "mongodbatlas_database_user" "db_user" {
   project_id = var.project_id
   username   = "matcheruser"
   password   = var.db_user_password
   roles {
-    role_name     = "RoleTest"
+    role_name     = "atlasAdmin"
     database_name = "matcherMongoDB"
   }
-  auth_database_name = "matcherMongoDB"
+  auth_database_name = "admin"
 }
 
 resource "mongodbatlas_project_ip_access_list" "whitelist" {

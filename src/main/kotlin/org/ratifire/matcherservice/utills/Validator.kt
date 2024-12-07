4471d978dacde1928c52c87bd0e90729c4b54943
Ivan Ivashchenko <ivachko2009@gmail.com>
2024-12-07 17:23:46 +0200

    PR changes

--- src/main/kotlin/org/ratifire/matcherservice/utills/Validator.kt
+++ src/main/kotlin/org/ratifire/matcherservice/utills/Validator.kt
@@ -1,8 +1,5 @@
 package org.ratifire.matcherservice.utills
 
 import org.ratifire.matcherservice.dto.ParticipantDto
-import java.util.Date
 
 fun validateParticipant(participant: ParticipantDto) = participant.dates.size >= participant.desiredInterview
-
-fun validateParticipant(availableDates: Set<Date>, desiredInterview: Int) = availableDates.size >= desiredInterview
